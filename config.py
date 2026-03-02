ccfdef5f3c8d0f4069d83ba9108a8b2ee422f01e
Rishu Pandey <55981293+theRishu@users.noreply.github.com>
2026-03-02 13:18:13 +0530

    new changes

--- config.py
+++ config.py
@@ -4,12 +4,4 @@ import sys
 BOT_NAME = sys.argv[2]
 BOT_TOKEN = sys.argv[1]
 WEB_URL = "https://lovetender.in"
-"""
-python main.py 1482537711:AAF3p2MSJIrHhUHVnvPXVr96m-MYCq4jFUA test
 
-
-BOT_NAME = "test"
-BOT_TOKEN = "1482537711:AAEn2ki_6QfJYhaT1Bh8pScmpV8E8_T7I4A"
-
-
-"""
